13d11108564c36fd2f7438a66f2bb9c64c94fb62
sougata das <sougata19582001@gmail.com>
2022-09-24 00:45:40 +0530

    added: error dumping file initialized

--- Main.py
+++ Main.py
@@ -2,8 +2,10 @@ import sys
 import os
 inpt = os.path.abspath(os.path.dirname(__file__) + "\\input.txt")
 output = os.path.abspath(os.path.dirname(__file__) + "\\output.txt")
+err = os.path.abspath(os.path.dirname(__file__) + "\\error.txt")
 sys.stdin = open(inpt, "r")
 sys.stdout = open(output, "w")
+sys.stderr = open(err, "w")
 
 
 def fibonacci(n, first=0, second=1):
